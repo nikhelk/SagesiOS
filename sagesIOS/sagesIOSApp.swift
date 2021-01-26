2b183e68526435e8fd2f1885ef311cb1b1f9507f
Nikhel Krishna <krishna.nikhel@paideiaschool.org>
2021-01-25 19:10:08 -0500

    Initial Commit

---
+++ sagesIOS/sagesIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  sagesIOSApp.swift
+//  sagesIOS
+//
+//  Created by Nikhel Krishna on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct sagesIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
